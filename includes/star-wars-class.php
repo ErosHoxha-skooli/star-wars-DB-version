0aaa409347d517f1650d95918aa026f21c1f5422
Eros Hoxha <eros@klassroom.com>
2021-12-31 11:41:18 +0100

    Moved code to html template files

--- includes/star-wars-class.php
+++ includes/star-wars-class.php
@@ -1,6 +1,7 @@
 <?php
 require_once(plugin_dir_path(__FILE__) . '/../star-wars.php');
 
+
 class Star_Wars_Widget extends WP_Widget
 {
     function __construct()
@@ -24,16 +25,16 @@ class Star_Wars_Widget extends WP_Widget
         $table_name = $wpdb->prefix . 'starwars';
         $db_results = $wpdb->get_results(" SELECT * FROM $table_name ");
         if (isset($_POST['nr_of_ships'])) {
-            $test = $_POST['nr_of_ships'];
-            $idtest = $_POST['id'];
+            $nr_of_ships = $_POST['nr_of_ships'];
+            $id = $_POST['id'];
             foreach ($db_results as $r) {
                 $wpdb->update(
                     $table_name,
                     array(
-                        'nr_of_ships' => $test,
+                        'nr_of_ships' => $nr_of_ships,
                     ),
                     array(
-                        'id' => $idtest,
+                        'id' => $id,
                     )
                 );
             }
@@ -53,14 +54,7 @@ class Star_Wars_Widget extends WP_Widget
         global $wpdb;
         $table_name = $wpdb->prefix . 'starwars';
         $db_results = $wpdb->get_results(" SELECT * FROM $table_name ");
-        echo '<div>';
-        echo '<select>';
-        echo '<option>Choose Ship</option>';
-        foreach ($db_results as $r) {
-            echo  "<option value='ship-{$r->id}' >$r->name</option>";
-        }
-        echo '</select>';
-        echo '</div>';
+        include('ship-selector-partial.php');
     }
 
     function render_ship_information()
@@ -69,25 +63,7 @@ class Star_Wars_Widget extends WP_Widget
         $table_name = $wpdb->prefix . 'starwars';
         $db_results = $wpdb->get_results(" SELECT * FROM $table_name ");
         foreach ($db_results as $r) {
-            echo "<form method='POST'>";
-            echo "<div class='ship-{$r->id} box' >" . PHP_EOL;
-            echo "<div class='id'>ID: $r->id</div>" . PHP_EOL;
-            echo "<input type='hidden' name='id' value='$r->id'>";
-            echo "<div class='manufacturer'>Manufacturer: $r->manufacturer</div>" . PHP_EOL;
-            echo "<div class='cost_in_credits'>Cost in credits: $r->cost_in_credits</div>" . PHP_EOL;
-            echo "<div class='length'>Length: $r->length</div>" . PHP_EOL;
-            echo "<div class='max_atmosphering_speed'>Max atmosphering speed: $r->max_atmosphering_speed</div>" . PHP_EOL;
-            echo "<div class='crew'>Crew: $r->crew</div>" . PHP_EOL;
-            echo "<div class='passengers'>Passengers: $r->passengers</div>" . PHP_EOL;
-            echo "<div class='cargo_capacity'>Cargo capacity: $r->cargo_capacity</div>" . PHP_EOL;
-            echo "<div class='consumables'>Consumables: $r->consumables</div>" . PHP_EOL;
-            echo "<div class='hyperdrive_rating'>Hyperdrive rating: $r->hyperdrive_rating</div>" . PHP_EOL;
-            echo "<div class='MGLT'>MGLT: $r->MGLT</div>" . PHP_EOL;
-            echo "<div class='starship_class'>Starship class: $r->starship_class</div>" . PHP_EOL;
-            echo '<label>' . "Number of ships: " . '</label>' . '<br>' . "<input type='number' min='0' name='nr_of_ships'>" . '<br>' . '<br>';
-            echo "<input type='submit' name='submit' value='Submit the form'/>";
-            echo '</form>';
-            echo '</div>';
+            include('ship-info-partial.php');
         }
     }
 }
